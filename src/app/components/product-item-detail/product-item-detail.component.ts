b58eee23207cd5464899899aa5ebba7f45643f82
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-11 17:50:14 +0530

    MYSTORE: Dynamically display product item detail component

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Product } from '../../models/Product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent implements OnInit {
+  id: number = 0;
+  product: Product;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0.0,
+      url: '',
+      description: '',
+    };
   }
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = +params.get('id')!;
+
+      this.productService.getProducts().subscribe((res) => {
+        const products: Product[] = res;
+
+        this.product = products.filter((product) => product.id === this.id)[0];
+      });
+    });
+  }
 }
