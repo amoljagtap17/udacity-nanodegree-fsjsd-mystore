19eae0c0956b325b17cbd48f2d9e5e653bc67e8e
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-04-12 20:32:19 +0530

    MYSTORE: Make cart checkout form dynamic

--- src/app/components/cart-checkout-form/cart-checkout-form.component.ts
+++ src/app/components/cart-checkout-form/cart-checkout-form.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart-checkout-form',
   templateUrl: './cart-checkout-form.component.html',
-  styleUrls: ['./cart-checkout-form.component.css']
+  styleUrls: ['./cart-checkout-form.component.css'],
 })
 export class CartCheckoutFormComponent implements OnInit {
+  fullName: string = '';
+  address: string = '';
+  card: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitForm(): void {
+    const checkoutData = {
+      fullName: this.fullName,
+      address: this.address,
+      card: this.card,
+    };
+
+    this.fullName = '';
+    this.address = '';
+    this.card = '';
   }
-
 }
